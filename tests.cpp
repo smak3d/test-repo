18c526c87d3b14c3a6595e964067a7e39c9948de
smak3d <85807850+smak3d@users.noreply.github.com>
2024-12-09 10:02:58 +0300

    add tests.h & tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,1 +1,92 @@
-﻿
+﻿#include "tests.h"
+
+//body_contains
+bool testBodyContainsGreen() {}
+bool testBodyContainsGreenBoundary(){}
+bool testBodyContainsRed(){}
+
+//contains
+bool testContainsInsideBounds(){}
+bool testContainsOnBounds(){}
+bool testContainsOutsideBounds(){}
+
+//full_size
+bool testFullSizePositive(){}
+bool testFullSizeNegative(){}
+bool testFullSizeZero(){}
+
+//body_size
+bool testBodySizeGreen(){}
+bool testBodySizeRed(){}
+bool testBodySizeZero(){}
+
+//is_red
+bool testIsRedTrue(){}
+bool testIsRedFalse(){}
+bool testIsRedZeroBody(){}
+
+//is_green
+bool testIsGreenTrue(){}
+bool testIsGreenFalse(){}
+bool testIsGreenZeroBody(){}
+
+//
+
+void initTests()
+{
+     tests = {
+        testBodyContainsGreen,
+        testBodyContainsGreenBoundary,
+        testBodyContainsRed,
+        testContainsInsideBounds,
+        testContainsOnBounds,
+        testContainsOutsideBounds,
+        testFullSizePositive,
+        testFullSizeNegative,
+        testFullSizeZero,
+        testBodySizeGreen,
+        testBodySizeRed,
+        testBodySizeZero,
+        testIsRedTrue,
+        testIsRedFalse,
+        testIsRedZeroBody,
+        testIsGreenTrue,
+        testIsGreenFalse,
+        testIsGreenZeroBody
+    };
+
+    auto addTests = [&]() {
+        for (const auto& testFunc : tests)
+        {
+            tests.push_back(testFunc);
+        }
+    };
+
+    addTests();
+}
+
+int launchTests()
+{
+    int total = 0;
+    int passed = 0;
+
+    for (const auto& test : tests)
+    {
+        std::cout << "test #" << (total + 1);
+        if (test())
+        {
+            passed += 1;
+            std::cout << " passed\n";
+        }
+        else
+        {
+            std::cout << " failed\n";
+        }
+        total += 1;
+    }
+
+    std::cout << "\ntests " << passed << "/" << total << " passed!" << std::endl;
+
+    //0 = success
+    return total - passed;
+}
--- tests.h
+++ tests.h
@@ -1,1 +1,42 @@
-﻿#include "candle.h"
+﻿#include <functional>
+#include <vector>
+#include <iostream>
+
+#include "candle.h"
+
+static std::vector<std::function<bool()>> tests;
+
+//body_contains
+bool testBodyContainsGreen();
+bool testBodyContainsGreenBoundary();
+bool testBodyContainsRed();
+
+//contains
+bool testContainsInsideBounds();
+bool testContainsOnBounds();
+bool testContainsOutsideBounds();
+
+//full_size
+bool testFullSizePositive();
+bool testFullSizeNegative();
+bool testFullSizeZero();
+
+//body_size
+bool testBodySizeGreen();
+bool testBodySizeRed();
+bool testBodySizeZero();
+
+//is_red
+bool testIsRedTrue();
+bool testIsRedFalse();
+bool testIsRedZeroBody();
+
+//is_green
+bool testIsGreenTrue();
+bool testIsGreenFalse();
+bool testIsGreenZeroBody();
+
+//
+void initTests();
+
+int launchTests();
